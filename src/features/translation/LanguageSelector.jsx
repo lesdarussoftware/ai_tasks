cbf92048bc15bac6dc567eb7a5a60f1abd1d25b5
matias ramos <matiasramos261@gmail.com>
2024-02-08 20:03:36 -0300

    fixes

--- src/features/translation/LanguageSelector.jsx
+++ src/features/translation/LanguageSelector.jsx
@@ -5,9 +5,15 @@ export default function LanguageSelector({ type, onChange, defaultLanguage }) {
         <div className='languageSelector'>
             <label>{type} </label>
             <select onChange={onChange} value={defaultLanguage}>
-                {Object.entries(LANGUAGES).map(([key, value]) => {
-                    return <option key={key} value={value}>{LABELS[key]}</option>
-                })}
+                {Object.entries(LANGUAGES)
+                    .sort((a, b) => {
+                        if (LABELS[a[0]].charAt(0).replace('Á', 'A') > LABELS[b[0]].charAt(0).replace('Á', 'A')) return 1
+                        if (LABELS[a[0]].charAt(0).replace('Á', 'A') < LABELS[b[0]].charAt(0).replace('Á', 'A')) return -1
+                        return 0
+                    })
+                    .map(([key, value]) => {
+                        return <option key={key} value={value}>{LABELS[key]}</option>
+                    })}
             </select>
         </div>
     )
